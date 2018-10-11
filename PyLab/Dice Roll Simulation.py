8ac8d5a489b836926e32643c06c045d7570fe92f
Shreyash Shrivastava <shreyashshrivastava@Shreyashs-MacBook-Pro.local>
2018-10-11 06:27:21 -0500

    Fixed Dice Roll

--- PyLab/Dice Roll Simulation.py
+++ PyLab/Dice Roll Simulation.py
@@ -6,7 +6,7 @@ import pylab as py
 values = []
 def printDice(n):
     for i in range(n):
-        values.append(random.choice([0.0,1,],0.5))
+        values.append(random.choice([0,1,2,3,4,5]))
         print str(values[i]) + '',
 
 
